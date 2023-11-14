7f9c6a3d914ae38ba24a91b907b20cf642da09d0
Zurisaday Espadas <zuryespadas@Zurisadays-MacBook-Air.local>
2023-11-14 15:16:10 -0600

    User

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -58,20 +58,26 @@ export const POST = async (req: Request) => {
 			evt.data || {};
 
 		try {
-			await fetch('https://collector-bookstore.vercel.app/api/users', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					clerkId: id,
-					name: `${first_name} ${last_name}`,
-					email: email_addresses[0].email_address,
-					image: image_url,
-				}),
-			});
+			const newUser = await fetch(
+				'https://collector-bookstore.vercel.app/api/users',
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						clerkId: id,
+						name: `${first_name} ${last_name}`,
+						email: email_addresses[0].email_address,
+						image: image_url,
+					}),
+				}
+			);
 
-			return NextResponse.json({ message: 'User created' }, { status: 201 });
+			return NextResponse.json(
+				{ message: `User created ${newUser}` },
+				{ status: 201 }
+			);
 		} catch (err) {
 			return NextResponse.json(
 				{ message: `Internal Server Error ${err}` },
